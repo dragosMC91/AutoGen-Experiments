7dcc1db698a1b6e0685a7a15b1f4e046aa226fbd
Dragos Campean <dragos@sectorlabs.ro>
2023-11-08 22:04:16 +0200

    Also accept custom strings in code_reviewer not just files

--- applications/code_reviewer.py
+++ applications/code_reviewer.py
@@ -2,6 +2,10 @@ from agents import custom_agents
 from sys import argv
 from operator import itemgetter
 
+code_to_review = """
+print("hello world")
+"""
+
 user_proxy, openai_coder = itemgetter('user_proxy', 'openai_coder')(
     custom_agents.get_agents()
 )
@@ -14,5 +18,7 @@ user_proxy.initiate_chat(
     ```````````````````````````
     {argv[1]}
     ```````````````````````````
-    """,
+    """
+    if len(argv) > 1
+    else code_to_review,
 )
